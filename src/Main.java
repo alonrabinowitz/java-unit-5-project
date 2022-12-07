7b9dfbbeac2f8f34feebeb1855c202ad76e21471
alonrabinowitz <62042858+alonrabinowitz@users.noreply.github.com>
2022-12-07 11:52:25 -0800

    Constructor arguments

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class Main extends PApplet {
-    Paper p;
-    Rock r;
-    Scissors s;
+    ArrayList<Rock> rocks;
+    ArrayList<Paper> papers;
+    ArrayList<Scissors> scissors;
+
     public void settings(){
         size(600,600);
     }
@@ -13,17 +16,44 @@ public class Main extends PApplet {
     s = new Scissors(3,3,3,3,loadImage("scissors.png"));
     }
     public void draw(){
-    image(p.p1, 300,300);
-    image(s.p1, 500,500);
-    image(r.p1,100,100);
-    p.move();
-    r.move();
-    s.move();
-    p.act();
-    s.act();
-    r.act();
+        for(Paper currPaper : papers){
+            currPaper.act();
+            currPaper.move();
+            image(currPaper.p1, currPaper.x, currPaper.y);
+        }
+        for(Scissors currScissors : scissors){
+            currScissors.act();
+            currScissors.move();
+            image(currScissors.p1, currScissors.x, currScissors.y);
+        }
+        for(Rock currRock : rocks){
+            currRock.act();
+            currRock.move();
+            image(currRock.p1, currRock.x, currRock.y);
+        }
     }
 
+    public void addRock(){
+        int x = (int)(Math.random()*600);
+        int y = (int)(Math.random()*600);
+        int xs = (int)(Math.random()*8+2);
+        int ys = (int)(Math.random()*8+2);
+        rocks.add(new Rock(x,y,xs,ys,loadImage("rock.png")));
+    }
+    public void addPaper(){
+        int x = (int)(Math.random()*600);
+        int y = (int)(Math.random()*600);
+        int xs = (int)(Math.random()*8+2);
+        int ys = (int)(Math.random()*8+2);
+        papers.add(new Paper(x,y,xs,ys,loadImage("paper.png")));
+    }
+    public void addScissors(){
+        int x = (int)(Math.random()*600);
+        int y = (int)(Math.random()*600);
+        int xs = (int)(Math.random()*8+2);
+        int ys = (int)(Math.random()*8+2);
+        scissors.add(new Scissors(x,y,xs,ys,loadImage("scissors.png")));
+    }
     public static void main(String[] args) {
         PApplet.main("Main");
     }
