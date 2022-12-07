710ee8b6685479408ef3e91a1e455b637fe3b7d5
Sidharth Shreedhar <mckidwizx@gmail.com>
2022-12-06 19:35:20 -0800

    Config of Object Classes

---
+++ src/Rock.java
@@ -0,0 +1,27 @@
+public class Rock {
+    int x,y,xs,ys, iW, iH;
+    public Rock(){
+        this.x = x;
+        this.y = y;
+        this.xs = xs;
+        this.ys = ys;
+    }
+    public void reverseX(){
+        xs = -xs;
+    }
+    public void reverseY(){
+        ys = -ys;
+    }
+    public void move(){
+        x = (int)(Math.random()*(600-iW));
+        y = (int)(Math.random()*(600-iH));
+    }
+    public void act(){
+        if(x < 0 || x>=600){
+            reverseX();
+        }
+        if(y < 0 || y >= 600){
+            reverseY();
+        }
+    }
+}
